2443228beede0fd10f550e68d8391aece1b10383
Brian Walters <brian.walters@onshoreoutsourcing.com>
2020-02-18 11:59:15 -0500

    after day one

--- EmployeeTracker/Program.cs
+++ EmployeeTracker/Program.cs
@@ -6,11 +6,58 @@ using System.Threading.Tasks;
 
 namespace EmployeeTracker
 {
+    enum GenderTypes
+    {
+        Male,
+        Female,
+        NotDisclosed,
+    }
+    enum VeteranTypes
+    {
+        Veteran,
+        NotAVeteran,
+        NotDisclosed,
+    }
+    class person
+    {
+        public int age;
+        public string name;
+        public string ssn;
+        public GenderTypes gender;
+        public VeteranTypes veteranStatus;
+        public override string ToString()
+        {
+            return $"age:{age} name:{name}";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            person p1;
+            person p2;
+            person p3;
 
+            p1 = new person();
+            p2 = new person();
+            p3 = p1;
+
+            p1.age = 10;
+            p1.name = "Brian";
+            p1.ssn = "000-00-0000";
+            p1.gender = GenderTypes.Male;
+            p1.veteranStatus = VeteranTypes.NotDisclosed;
+
+            p2.age = 20;
+            p2.name = "Carol";
+            p2.ssn = "000-00-0000";
+            p2.gender = GenderTypes.Female;
+            p2.veteranStatus = VeteranTypes.NotAVeteran;
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
         }
     }
 }
